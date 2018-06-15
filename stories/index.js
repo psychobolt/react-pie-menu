548e3f242741c72f1a38e04ed13efed420cf2b73
psychobolt <psychobolt@gmail.com>
2018-06-15 09:48:52 -0700

    Include Readme in storybook

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { doc } from 'storybook-readme';
 
+import Readme from '../README.md';
 import ContextMenu from './ContextMenu';
 import PreferenceSelector from './PreferenceSelector';
 
 storiesOf('PieMenu', module)
+  .add('Readme', doc(Readme))
   .add('as Context Menu', () => <ContextMenu />)
   .add('with Submenus', () => <PreferenceSelector />);
