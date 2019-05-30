a0ebf75d190a2e5345902252e1079f4bdab4c167
psychobolt <psychobolt@gmail.com>
2019-05-30 15:31:24 -0700

    Upgrade packages

--- src/Slice/index.js
+++ src/Slice/index.js
@@ -1,3 +1,5 @@
-export * as sliceStyles from './Slice.style';
+import * as sliceStyles from './Slice.style';
+
+export { sliceStyles };
 export * from './Slice.component';
 export { default } from './Slice.component';
