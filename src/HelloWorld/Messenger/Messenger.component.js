4d69c8a92f58a499b0ab6dfd29640d9fb549b10d
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-03-15 20:18:50 -0700

    improve code coverage

--- src/HelloWorld/Messenger/Messenger.component.js
+++ src/HelloWorld/Messenger/Messenger.component.js
@@ -1,4 +1,5 @@
 export default () => {
+  /* istanbul ignore next */
   if (process.env.NODE_ENV === 'development') {
     console.log('DEVELOPMENT sources are loaded'); // eslint-disable-line no-console
   }
