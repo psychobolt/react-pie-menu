5543ce51717ad7a5fb64d1665f53713a4dd316bc
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-03-24 20:36:31 -0700

    postinstall flow types

--- flow-typed-update.js
+++ flow-typed-update.js
@@ -1,26 +1,39 @@
+/* eslint-disable no-console */
+import fs from 'fs';
+import path from 'path';
 import spawn from 'cross-spawn';
 
+// TODO use project resolver
 import { setup, getProjects, getPackageName } from './workspaces.js';
-import { rootResolve } from './shared/utils.js';
+import { rootResolve, dirname } from './shared/utils.js';
+
+const ROOT_RESOLVE = rootResolve();
+const SHARED_RESOLVE = path.resolve(ROOT_RESOLVE, 'shared');
+const FLOW_DEPS_RESOLVE = path.resolve(SHARED_RESOLVE, 'flow-deps');
+const FLOW_DEPS_LINK_RESOLVE = path.resolve(ROOT_RESOLVE, 'flow-deps-modules');
+
+const command = (args, cwd) => spawn.sync('yarn', args, { stdio: 'inherit', cwd });
+
+if (!fs.existsSync(FLOW_DEPS_LINK_RESOLVE)) {
+  console.log('Installing Flow Dependencies...');
+  command(['install'], FLOW_DEPS_RESOLVE);
+  command(['symlink-dir', path.resolve(ROOT_RESOLVE, FLOW_DEPS_RESOLVE, 'node_modules'), FLOW_DEPS_LINK_RESOLVE]);
+}
 
 await (setup());
 const projects = await (getProjects());
 
-const ROOT_RESOLVE = rootResolve();
+const libdefDir = path.relative(dirname(import/*:: ("") */.meta.url), path.resolve(SHARED_RESOLVE, 'flow-typed'));
 
-const libdefDir = './shared/flow-typed';
+console.log(libdefDir);
 
-/* eslint-disable no-console, no-await-in-loop, no-restricted-syntax */
+/* eslint-disable no-await-in-loop, no-restricted-syntax */
 for (const [cwd] of projects) {
   const name = await (getPackageName(cwd));
-  console.log(`Installing flow types for [${name}]`);
-  spawn.sync(
-    'yarn',
-    [
-      'workspace', name,
-      'exec', 'flow-typed', 'update', '--libdefDir', libdefDir, '-s', '-i', 'dev', '-p', ROOT_RESOLVE, '--verbose', '--skipFlowRestart',
-    ],
-    { stdio: 'inherit' },
-  );
+  console.log(`Checking flow types for [${name}]`);
+  command([
+    'workspace', name,
+    'exec', 'flow-typed', 'update', '--libdefDir', libdefDir, '-s', '--skipFlowRestart', '-i', 'dev', '-p', ROOT_RESOLVE,
+  ]);
   console.log();
 }
