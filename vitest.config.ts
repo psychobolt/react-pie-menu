0afba35da1a72b4f51d8a34aa14f123d355c1631
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-06-28 15:24:38 -0700

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,25 +6,27 @@ import viteConfig from './vite.config.js';
 
 const require = createRequire(import.meta.url);
 
-export default mergeConfig(
-  mergeConfig(viteConfig, commonConfig),
-  defineConfig({
-    test: {
-      coverage: {
-        include: ['src/**/*.{ts,tsx}']
-      },
-      projects: [
-        '.storybook/vitest.config.ts',
-        {
-          extends: true,
-          test: {
-            name: 'react (jsdom)',
-            environment: 'jsdom',
-            setupFiles: [require.resolve('commons/esm/vitest.setup')]
+export default defineConfig((env) =>
+  mergeConfig(
+    mergeConfig(commonConfig, viteConfig(env)),
+    defineConfig({
+      test: {
+        coverage: {
+          include: ['src/**/*.{ts,tsx}']
+        },
+        projects: [
+          '.storybook/vitest.config.ts',
+          {
+            extends: true,
+            test: {
+              name: 'react (jsdom)',
+              environment: 'jsdom',
+              setupFiles: [require.resolve('commons/esm/vitest.setup')]
+            }
           }
-        }
-      ],
-      passWithNoTests: true
-    }
-  })
+        ],
+        passWithNoTests: true
+      }
+    })
+  )
 );
